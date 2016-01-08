9b3fed8e5f7303864414d8f9a9dfb2c6b4f012dd
Daehyun Kim <hatemogi@gmail.com>
2016-01-08 16:53:08 +0900

    special-form은 매크로로 대치 가능해 보인다.

--- src/klojure/core.clj
+++ src/klojure/core.clj
@@ -2,47 +2,29 @@
       :author "김대현"}
     klojure.core)
 
-(def
-  ^{:arglists '([& 요소들])
-    :doc "요소들을 담고 있는 리스트를 새로 만든다."
-    :added "1.0"}
-  리스트 (. clojure.lang.PersistentList creator))
-
-(def
-  ^{:macro true
-    :added "1.0"}
-  가정 (fn* 가정 [&form &env & decl] (cons 'let* decl)))
-
-(def
-  ^{:macro true
-    :added "1.0"}
-  루프 (fn* 루프 [&form &env & decl] (cons 'loop* decl)))
-
-(def
-  ^{:macro true
-    :added "1.0"}
-  함 (fn* 함 [&form &env & decl]
-          (.withMeta ^clojure.lang.IObj (cons 'fn* decl)
-                     (.meta ^clojure.lang.IMeta &form))))
-
-(defmacro 매크로
-  "defmacro를 그대로 사용"
-  [& 선언부]
-  (cons 'defmacro 선언부))
-
-(매크로 대응
-  "단순한 1:1 대응을 위한 매크로. 단순히 매크로로 치환된다."
+(defmacro 대응
+  "단순한 1:1 대응을 위한 매크로. 단순히 매크로로 치환한다."
   [한글명 심볼]
-  (리스트 '매크로 한글명 '[& 선언부]
-    (리스트 'cons (리스트 'quote 심볼) '선언부)))
+  (list 'defmacro 한글명
+        (select-keys (meta (var defmacro)) [:doc])
+        '[& 선언부]
+        (list 'cons (list 'quote 심볼) '선언부)))
 
+(대응 매크로 defmacro)
 (대응 매크로확장 macroexpand)
 (대응 매크로확장-1 macroexpand-1)
-
+(대응 인용 quote)
+(대응 가정 let)
+(대응 함 fn)
 (대응 정의 def)
 (대응 함수 defn)
 (대응 함수- defn-)
 (대응 만약 if)
 
+(대응 루프 loop)
+(대응 재귀 recur)
+
+(정의 리스트 list)
 (정의 증가 inc)
 (정의 감소 dec)
+(정의 맵 map)
