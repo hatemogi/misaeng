1c1b2102be5bd16ef5212356b30766755c9e129c
Daehyun Kim <hatemogi@gmail.com>
2016-01-13 11:16:10 +0900

    프로젝트명 변경: klojure -> misaeng

--- src/user.clj
+++ src/user.clj
@@ -1,4 +1,5 @@
 (ns user
   (:require #_[klojure.core :refer :all]
             #_[klojure.test :refer :all]
+            [clojure.repl :refer :all]
             [clojure.tools.namespace.repl :refer [refresh]]))
--- test/klojure/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns klojure.core-test
-  (:require [klojure.core :refer :all]
-            [klojure.test :refer :all]))
-
-(실험함수 클로저핵심
-  (실험 "실패하는 테스트. 고쳐보세요."
-    (확인 (= 0 1))))
