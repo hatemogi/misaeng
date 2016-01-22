2605e57a4843c4ea4b9bfae908d60217df4a586a
Daehyun Kim <hatemogi@gmail.com>
2016-01-22 21:41:44 +0900

    클로저 업그레이드 -> 1.8.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject misaeng "0.0.1"
+(defproject misaeng "0.1.0"
   :description "한글 클로저 실험"
   :url "https://github.com/hatemogi/misaeng"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.namespace "0.2.9"]])
