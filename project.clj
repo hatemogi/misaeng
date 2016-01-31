a1e27ee7b78f06bfdd2a454bb648dbf0a4cf0df7
Daehyun Kim <hatemogi@gmail.com>
2016-01-31 15:51:40 +0900

    클로저스크립트도 고려합니다.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.namespace "0.2.9"]]
+                 [org.clojure/tools.namespace "0.2.9"]
+                 [org.clojure/clojurescript "1.7.228"]]
   :test-paths ["test" "src/미생/예제"])
