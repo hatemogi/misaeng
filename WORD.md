ec532ceaf9e0b2cf9bf655c9f9699c1bb652630e
Daehyun Kim <hatemogi@gmail.com>
2016-03-17 00:35:42 +0900

    ...

---
+++ WORD.md
@@ -0,0 +1,5 @@
+let 가정
+expression 표현
+function 함수
+binding 바인딩
+parameter 매개변수
---
+++ src/미생/레플.clj
@@ -0,0 +1,11 @@
+(ns 미생.레플
+  (:use [미생.기본])
+  (:require [clojure.repl]))
+
+(매크로대응 설명 clojure.repl/doc)
+(매크로대응 소스 clojure.repl/source)
+(매크로대응 열람 clojure.repl/dir)
+(매크로대응 설명찾기 clojure.repl/find-doc)
+(매크로대응 정의현황 clojure.repl/apropos)
+(정의 pst clojure.repl/pst)
+(정의 demunge clojure.repl/demunge)
