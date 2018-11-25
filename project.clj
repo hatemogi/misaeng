9f32b9745fbe7bc7047f13691eaf0987de4afe6a
김대현 <hatemogi@gmail.com>
2018-11-25 14:12:37 +0900

    클로저 1.9 버전업

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/hatemogi/misaeng"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :scm {:name "git"
         :url "https://github.com/hatemogi/misaeng"}
   :deploy-repositories [["releases" :clojars]]
